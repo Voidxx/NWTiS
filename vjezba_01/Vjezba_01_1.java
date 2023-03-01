dec432f06db897346dcf6df16f29c9278a934f6d
Leon Sedlanić <lsedlanic@foi.hr>
2023-03-01 08:30:26 +0100

    1.vjezba - sređeni primjer 01_1

--- vjezba_01/Vjezba_01_1.java
+++ vjezba_01/Vjezba_01_1.java
@@ -1,4 +1,7 @@
-total 8
-drwxr-xr-x 2 NWTiS_1 users 4096 Mar  1 08:13 .
-drwxr-xr-x 4 NWTiS_1 users 4096 Mar  1 08:12 ..
--rw-r--r-- 1 NWTiS_1 users    0 Mar  1 08:13 Vjezba_01_1.java
+public class Vjezba_01_1 {
+ public static void main(String[] args) {
+ for(String a : args){
+ System.out.print(a);
+ }
+ }
+}
