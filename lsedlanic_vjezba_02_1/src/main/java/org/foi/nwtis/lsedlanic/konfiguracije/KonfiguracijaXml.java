0e78cac44c3b529f881a708e36a9cbe579a903cf
Leon Sedlanić <lsedlanic@foi.hr>
2023-03-08 12:32:54 +0100

    Vjezba_02_3

--- lsedlanic_vjezba_02_1/src/main/java/org/foi/nwtis/lsedlanic/konfiguracije/KonfiguracijaXml.java
+++ lsedlanic_vjezba_02_1/src/main/java/org/foi/nwtis/lsedlanic/konfiguracije/KonfiguracijaXml.java
@@ -1,5 +1,10 @@
 package org.foi.nwtis.lsedlanic.konfiguracije;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.foi.nwtis.Konfiguracija;
 import org.foi.nwtis.KonfiguracijaApstraktna;
 import org.foi.nwtis.NeispravnaKonfiguracija;
 
@@ -13,13 +18,38 @@ public class KonfiguracijaXml extends KonfiguracijaApstraktna {
 
 	@Override
 	public void spremiKonfiguraciju(String datoteka) throws NeispravnaKonfiguracija {
-		// TODO Auto-generated method stub
+		var putanja = Path.of(datoteka);
+		var tip = Konfiguracija.dajTipKonfiguracije(datoteka);
+		if (tip == null || tip.compareTo(TIP) != 0) {
+			throw new NeispravnaKonfiguracija("Datoteka '" + datoteka + "' nije ispravnog tipa: '" + TIP + "'");
+		} else if (Files.exists(putanja) && (Files.isDirectory(putanja) || !Files.isWritable(putanja)))
+			throw new NeispravnaKonfiguracija(
+					"Datoteka '" + datoteka + "' je direktorij ili nije moguće pisati u nju.");
+
+		try {
+			this.postavke.storeToXML(Files.newOutputStream(putanja), "NWTIS lsedlanic 2023.");
+		} catch (IOException e) {
+			throw new NeispravnaKonfiguracija("Datoteka '" + datoteka + "' nije moguće pisati." + e.getMessage());
+		}
 
 	}
 
 	@Override
 	public void ucitajKonfiguraciju() throws NeispravnaKonfiguracija {
-		// TODO Auto-generated method stub
+		var datoteka = this.nazivDatoteke;
+		var putanja = Path.of(datoteka);
+		var tip = Konfiguracija.dajTipKonfiguracije(datoteka);
+		if (tip == null || tip.compareTo(TIP) != 0) {
+			throw new NeispravnaKonfiguracija("Datoteka '" + datoteka + "' nije ispravnog tipa: '" + TIP + "'");
+		} else if (Files.exists(putanja) && (Files.isDirectory(putanja) || !Files.isReadable(putanja)))
+			throw new NeispravnaKonfiguracija(
+					"Datoteka '" + datoteka + "' je direktorij ili nije moguće čitati iz nje.");
+
+		try {
+			this.postavke.loadFromXML(Files.newInputStream(putanja));
+		} catch (IOException e) {
+			throw new NeispravnaKonfiguracija("Datoteka '" + datoteka + "' nije moguće čitati." + e.getMessage());
+		}
 
 	}
 
